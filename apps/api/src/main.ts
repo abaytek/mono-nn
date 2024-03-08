d7104e8684d155863e18de240d3efde91f42f117
abaytek <abaysewtekle@gmail.com>
2024-03-08 07:30:47 +0000

    feat: added sample library

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { add } from '@mono/sample-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(add());
   await app.listen(3000);
 }
 bootstrap();
